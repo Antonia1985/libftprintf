457c8c28d7f1a6b5814c7b8d8725ce278077b8bd
Antonia <33874536+Antonia1985@users.noreply.github.com>
2024-12-05 18:39:39 +0100

    Update libftprintf.h

--- include/libftprintf.h
+++ include/libftprintf.h
@@ -1,13 +1,26 @@
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavlopo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 15:38:07 by apavlopo          #+#    #+#             */
+/*   Updated: 2024/12/05 15:38:35 by apavlopo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <unistd.h>
 
-void    ft_dec_to_hex(unsigned long n, const char *base, int *counter);
+void	ft_dec_to_hex(unsigned long n, const char *base, int *counter);
 void	ft_print_nbr(int n, int *counter);
+void	ft_print_ptr(unsigned long n, const char *base, int *counter);
 void	ft_print_unsigned_dec(unsigned int n, int *counter);
-int         ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 void	ft_putchar(char c, int *counter);
 void	ft_putstr(char *s, int *counter);
 
